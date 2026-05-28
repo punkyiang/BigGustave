fed1fd58916d047088f30e20350f8d51eead7318
PunkyIANG <tsoorckanukris@gmail.com>
2026-05-28 14:22:54 +0300

    more fixes

--- src/SierpinskiTriangle/TriangleGif.cs
+++ src/SierpinskiTriangle/TriangleGif.cs
@@ -11,52 +11,55 @@ public static class TriangleGif
         const int maxCycles = 11;
         int width = (int)Math.Pow(2, maxCycles);
         int height = width;
+        
+        var builder = PngBuilder.Create(width, height, true);
+            
+        var gold = new Pixel(192);
+        var transparent = new Pixel(0);
 
 
         // using var gif = AnimatedGif.AnimatedGif.Create("mygif.gif", 200);
 
-        for (int index = 0; index < 5; index++)
+        for (int index = 0; index < maxCycles; index++)
         {
-            var builder = PngBuilder.Create(width, height, false);
-
-            var black = new Pixel(255);
-            var white = new Pixel(0);
-
             for (int j = 0; j < width; j++)
             for (int k = 0; k < height; k++)
-                builder.SetPixel(black, j, k);
+                builder.SetPixel(gold, j, k);
 
-            DrawRecursively(0, new Point(0, 0), new Point(width, height), 4, index);
-            var filename = $"file{index}.png";
-            File.WriteAllBytes(filename, builder.Save());
+            DrawRecursively(0, new Point(width, 0), new Point(0, height), index);
+            var filename = $"silverblack{index}.png";
+            var bytes = builder.Save();
+            File.WriteAllBytes(filename, bytes);
+            builder = PngBuilder.FromPngBytes(bytes);
             // var img = Image.FromFile(filename);
             // gif.AddFrame(img, quality: GifQuality.Grayscale);
-            
-            void DrawRecursively(int cycle, Point outerCorner, Point innerCorner, int squareToIgnore, int maxCycleCount) {
-                if (cycle >= maxCycleCount) return;
-
-                int minX = outerCorner.X;
-                int minY = outerCorner.Y;
-                int midX = (outerCorner.X + innerCorner.X) / 2;
-                int midY = (outerCorner.Y + innerCorner.Y) / 2;
-                int maxX = innerCorner.X < width ? innerCorner.X : width - 1;
-                int maxY = innerCorner.Y < height ? innerCorner.Y : height - 1;
-    
-                // builder.DrawVerticalLine(white, midX, minY, midY);
-                // builder.DrawHorizontalLine(white, minX, midX, midY);
-    
-                builder.DrawRectangle(white, new Point(midX, midY), new Point(maxX, maxY));
-
-                if (squareToIgnore != 1) DrawRecursively(cycle + 1, new Point(minX, minY), new Point(midX, midY), squareToIgnore, maxCycleCount);
-                if (squareToIgnore != 2) DrawRecursively(cycle + 1, new Point(midX, minY), new Point(maxX, midY), squareToIgnore, maxCycleCount);
-                if (squareToIgnore != 3) DrawRecursively(cycle + 1, new Point(minX, midY), new Point(midX, maxY), squareToIgnore, maxCycleCount);
-                if (squareToIgnore != 4) DrawRecursively(cycle + 1, new Point(midX, midY), new Point(maxX, maxY), squareToIgnore, maxCycleCount);
-            }
         }
         
         return;
-    }
+        
+        void DrawRecursively(int cycle, Point outerCorner, Point innerCorner, int maxCycleCount) {
+            if (cycle >= maxCycleCount) return;
 
+            int minX = outerCorner.X;
+            int minY = outerCorner.Y;
+            int midX = (outerCorner.X + innerCorner.X) / 2;
+            int midY = (outerCorner.Y + innerCorner.Y) / 2;
+            int maxX = innerCorner.X;// < width ? innerCorner.X : width - 1;
+            int maxY = innerCorner.Y;// < height ? innerCorner.Y : height - 1;
+            
+            int maxXToDraw = maxX < width ? maxX : width - 1;
+            int maxYToDraw = maxY < height ? maxY : height - 1;
+    
+            // builder.DrawVerticalLine(white, midX, minY, midY);
+            // builder.DrawHorizontalLine(white, minX, midX, midY);
+    
+            builder.DrawRectangle(transparent, new Point(midX, midY), new Point(maxXToDraw, maxYToDraw));
+
+            DrawRecursively(cycle + 1, new Point(minX, minY), new Point(midX, midY), maxCycleCount);
+            DrawRecursively(cycle + 1, new Point(midX, minY), new Point(maxX, midY), maxCycleCount);
+            DrawRecursively(cycle + 1, new Point(minX, midY), new Point(midX, maxY), maxCycleCount);
+        }
+    }
 }
 
 public record Point(int X, int Y);
@@ -65,18 +68,27 @@ public static class PngExtensions
 {
     public static void DrawHorizontalLine(this PngBuilder builder, Pixel color, int xFrom, int xTo, int y)
     {
-        for (var x = xFrom; x <= xTo; x++) builder.SetPixel(color, x, y);
+        var minX = xFrom < xTo ? xFrom : xTo;
+        var maxX = xFrom > xTo ? xFrom : xTo;
+        for (var x = minX; x <= maxX; x++) builder.SetPixel(color, x, y);
     }
     
     public static void DrawVerticalLine(this PngBuilder builder, Pixel color, int x, int yFrom, int yTo)
     {
-        for (var y = yFrom; y <= yTo; y++) builder.SetPixel(color, x, y);
+        var minY = yFrom < yTo ? yFrom : yTo;
+        var maxY = yFrom > yTo ? yFrom : yTo;
+        for (var y = minY; y <= maxY; y++) builder.SetPixel(color, x, y);
     }
     
     public static void DrawRectangle(this PngBuilder builder, Pixel color, Point from, Point to)
     {
-        for (var x = from.X; x <= to.X; x++)
-        for (var y = from.Y; y <= to.Y; y++) 
+        var minX = from.X < to.X ? from.X : to.X;
+        var maxX = from.X > to.X ? from.X : to.X;
+        var minY = from.Y < to.Y ? from.Y : to.Y;
+        var maxY = from.Y > to.Y ? from.Y : to.Y;
+
+        for (var x = minX; x <= maxX; x++)
+        for (var y = minY; y <= maxY; y++) 
             builder.SetPixel(color, x, y);
     }
 }
